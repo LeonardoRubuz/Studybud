cc046c8cd40a4a473fea2a466c0c90fec8d96298
LeonardoRubuz <98391205+LeonardoRubuz@users.noreply.github.com>
2022-10-20 07:02:56 +0100

    Add files via upload

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -4,11 +4,17 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
+
 
     path('', views.home, name='home'),
     path('room/<int:id>', views.room, name='room'),
+    path('profile/<int:id>', views.userProfile, name='userprofile'),
+
     path('create-room/', views.createRoom, name='createroom'),
     path('update-room/<int:id>', views.updateRoom, name='updateroom'),
-    path('delete-room/<int:id>', views.deleteRoom, name='deleteroom')
+    path('delete-room/<int:id>', views.deleteRoom, name='deleteroom'),
+    path('delete-message/<int:id>', views.deleteMessage, name='deletemessage')
 
 ]
